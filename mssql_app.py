dd4feffe481020fa93b128ce7aa925170a5bd1b0
Abhishek Kumar <abhsihek_ku@yahoo.com>
2020-04-22 16:57:05 +0530

    added mssql mysql

--- mssql_app.py
+++ mssql_app.py
@@ -5,6 +5,6 @@ connection = pyodbc.connect('DSN=mssql;UID=mssql_admin;PWD=1nsecure')
 
 cursor = connection.cursor()
 
-cursor.execute("SELECT  name FROM guest.kpis;;")
+cursor.execute("SELECT  name FROM tempdb.guest.kpis;;")
 for row in cursor.fetchall():
     print(row.name)
